2cd27c46a7cacb91c2ae8843fc5f60c1f2581db9
longnv99 <litlong99@gmail.com>
2022-01-12 20:02:40 +0700

    update navbar mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,34 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import './Header.scss';
 import logo from '../../assets/images/tmdbIcon.svg';
 import { Link, useLocation } from 'react-router-dom';
 
 const headerNav = [
     {
-        display: 'Movies',
-        path: 'movie'
+        display: 'Watch list',
+        path: 'watchlist'
     },
     {
-        display: 'TV Shows',
-        path: 'tv'
+        display: 'Watched',
+        path: 'watched'
+    },
+    {
+        display: 'Sign In',
+        path: 'login'
+    },
+    {
+        display: 'Sign Up',
+        path: 'register'
     },
 ]
 
 const Header = () => {
     const { pathName } = useLocation();
     const headerRef = useRef(null);
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
 
     useEffect(() => {
         const shrinkHeader = () => {
@@ -34,23 +46,50 @@ const Header = () => {
 
     const active = headerNav.findIndex(nav => nav.path === pathName);
 
+    const width = window.innerWidth;
+    console.log(width);
+
     return (
         <div ref={headerRef} className='header'>
             <div className="header__wrap container">
                 <div className="logo">
                     <Link to='/'><img src={logo} alt="" /></Link>
                 </div>
-                <ul className='header__nav'>
-                    {
-                        headerNav.map( (nav, index) => (
-                            <li key={index} >
-                                <Link to={nav.path}>
-                                    {nav.display}
-                                </Link>
-                            </li>
-                        ))
+                {
+                        width > 740 ? (
+                            <ul className='header__nav'>
+                                {
+                                    headerNav.map( (nav, index) => (
+                                        <li key={index} >
+                                            <Link to={nav.path}>
+                                                {nav.display}
+                                            </Link>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        ) 
+                        : 
+                        (
+                            <>
+                                <i 
+                                    className={click ? 'fas fa-times' : 'fas fa-ellipsis-v' } 
+                                    onClick={handleClick}
+                                />
+                                <ul className={click ? "header__menus active" : "header__menus"}>
+                                    {
+                                        headerNav.map( (nav, index) => (
+                                            <li key={index} onClick={handleClick}>
+                                                <Link to={nav.path}>
+                                                    {nav.display}
+                                                </Link>
+                                            </li>
+                                        )) 
+                                    }
+                                 </ul>
+                            </>
+                        )
                     }
-                </ul>
             </div>
         </div>
     )
